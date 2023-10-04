a83fe750f4f59445b7efe6178f403cd1faa209aa
Vagif0728 <tu7j4ryqj@code.edu.az>
2023-10-05 03:47:16 +0400

    new task : all task install

---
+++ HomeTask5/Program.cs
@@ -0,0 +1,177 @@
+﻿using System.ComponentModel;
+using System.Reflection.PortableExecutable;
+
+namespace HomeTask5
+{
+    internal class Program
+    {
+        static void Main()
+        {
+
+            // --------------------------------------Task 1---------------------------------------
+
+
+            //Console.WriteLine("Arrayin elementlerini daxil edin :");
+            //int[] arr = new int[5];
+
+            //for (int i = 0; i <arr.Length; i++)
+            //{
+            //    arr[i] = int.Parse(Console.ReadLine());
+            //}
+
+            //Console.WriteLine("Arrayin en kicik elementi : ");
+            //Console.WriteLine(Min(arr));
+
+
+
+
+            //-----------------------------------------------Task2------------------------------------------
+
+            //Console.WriteLine("Olculeri daxil edin :");
+            //Console.WriteLine("Radius :");
+            //int r = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Teref a :");
+            //int a = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Teref b :");
+            //int b = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Teref c :");
+            //int c = int.Parse(Console.ReadLine());
+
+
+            //Console.WriteLine($" Cevrenin sahesi :{Area(r)}");
+            //Console.WriteLine($" Duzbucagin  sahesi :{Area(a, b)}");
+            //Console.WriteLine($" Duzbucaqli paralapipedin tam sethinin sahesi :{Area(a, b, c)}");
+            //Console.WriteLine($" Ucbucagin daxiline cekilmiw cevrenin sahesi :{Area(a, b, c, r)}");
+
+
+
+            //-------------------------------------------------------Task 3---------------------------------------------------------------
+            
+            
+            //Console.WriteLine("Ededi daxil edin :");
+            //int number = int.Parse(Console.ReadLine());
+
+            //bool checks = CheckOfNumber(number);
+
+            //CheckOfNumber(SumOfDigit(number));
+
+
+            //    if (checks == true)
+            //    {
+            //        Console.WriteLine(SumOfDigit(number));
+
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Eded 0 ve 0 dan boyuk eded olmalidi");
+            //    }
+                
+
+            
+
+
+
+
+
+        }
+
+
+        //-------------------------------------------------------Task 1----------------------------------------------------------------
+
+
+
+        //public static int Min(int [] arr)
+        //{
+        //    int min = arr[0];
+
+        //    for (int i = 0; i <arr.Length; i++)
+        //    {
+        //        if (min > arr[i])
+        //        {
+        //            min = arr[i];
+        //        }
+
+        //    }
+        //    return min;
+        //}
+
+
+        //-------------------------------------------------------Task 2---------------------------------------------------------------
+
+
+
+
+        //public static int Area(int r) 
+        //{
+        //    int p = 3;
+
+        //    int s = p * (r * r);
+        //    return s; 
+        //} 
+
+        //public static int Area(int a, int b)
+        //{
+        //    int s = a * b;
+        //    return s;   
+        //}
+
+        //public static int Area(int a,int b,int c)
+        //{
+        //    int s = 2*(a * b + a * c + b * c);
+        //    return s;
+        //}
+
+        //public static int Area(int a, int b, int c, int r)
+        //{
+        //    int p = (a + b + c) / 2;
+        //    int s = p * r;
+        //    return s;
+        //}
+
+
+
+
+
+
+
+        //-------------------------------------------------------Task 3--------------------------------------------------------------
+
+
+        
+
+
+        //public static bool CheckOfNumber(int number)
+        //{
+        //    return number > 0;
+        //}
+
+       
+
+
+        //public static int SumOfDigit(int number)
+        //{
+        //    int sum = 0;
+        //    while (number != 0)
+        //    {
+        //        sum += number % 10;
+        //        number /= 10;
+        //    }
+        //    return sum;
+        //}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+}
